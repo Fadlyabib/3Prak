a0f5a9f6faed6ab71b81b519a5c483d41be6fb3a
Fadlyabib <fadlyahabib05@gmail.com>
2022-12-05 10:30:47 +0700

    task P10

--- P10/latihan/async-download.js
+++ P10/latihan/async-download.js
@@ -3,23 +3,25 @@
  * Fungsi untuk menampilkan hasil download
  * @param {string} result - Nama file yang didownload
  */
- function showDownload(result) {
+ const showDownload = (result) => {
     console.log("Download selesai");
     console.log("Hasil Download: " + result);
-  }
+  };
   
   /**
    * Fungsi untuk download file
    * @param {function} callback - Function callback show
    */
-  function download(callShowDownload) {
-    setTimeout(function () {
-      const result = "windows-10.exe";
-      callShowDownload(result);
-    }, 3000);
-  }
+  const download = (callShowDownload) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const result = "windows-10.exe";
+            callShowDownload(result);
+        }, 3000);
+    });
+  };
   
-  download(showDownload);
+download(showDownload);
   
   /**
    * TODO:
