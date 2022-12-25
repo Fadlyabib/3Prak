5180e36762b912f2d64745bb59a970f36cfe41bd
Fadlyabib <91892354+Fadlyabib@users.noreply.github.com>
2022-12-25 22:28:20 +0700

    task 12

--- praktikum/controller/StudentController.js
+++ praktikum/controller/StudentController.js
@@ -1,35 +1,53 @@
 // import data student
-const students = require("../data/students.js")
+// const students = require("../data/students.js");
+
+// import model student
+const Student = require("../models/Student");
 
 // membuat class studentcontroller
 class StudentController{
-    index(req, res){
+    // index(req, res){
+    //     // memanggil method static all
+    //     Student.all(function(students){ 
+    //         const data = {
+    //             message: "Menampilkan semua students",
+    //             data: students,
+    //         };
+    //         res.json(data);
+    //     });
+    // }
+
+    // menambahkan keyword async untuk memberitahu proses asynchronous
+    async index(req, res){
+        // memanggil method static all dengan async await
+        const students = await Student.all();
         const data = {
             message: "Menampilkan semua students",
-            data: students
+            data: students,
         };
-
         res.json(data);
     }
 
-    store(req, res){
-        const {nama} = req.body;
-        students.push(nama);
+    async store(req, res){
+        /**
+         * method create mengembalikan data yang baru diinsert
+         * kembalikan data dalam bentuk json
+         */
+        const students = await Student.create(req.body);
         const data = {
-           message: `Menambahkan data students: ${nama}`,
+           message: "Menambahkan data students",
            data: students
         };
-
         res.json(data);
     }
 
     update(req, res){
         const {id} = req.params;
         const {nama} = req.body;
-        students[id] = nama;
+        Student[id] = nama;
         const data = {
             message: `Mengedit students id ${id}, nama ${nama}`,
-            data: students
+            data: Student
         };
 
         res.json(data);
@@ -37,10 +55,10 @@ class StudentController{
 
     destroy(req, res){
         const {id} = req.params;
-        students.splice(id, 1);
+        Student.splice(id, 1);
         const data = {
             message: `Menghapus students id ${id}`,
-            data: students
+            data: Student
         };
 
         res.json(data);
